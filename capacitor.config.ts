b894b50740f7203c283004e1b1a06a7a603fec24
unknown <jampasnando@gmail.com>
2022-03-05 12:23:22 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sistembo.test0322',
+  appName: 'test0322',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
